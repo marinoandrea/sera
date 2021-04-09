98f8a2b05593fa5f5ece004a7a0c5316e1473586
Andrea Marino <am.marinoandrea@gmail.com>
2021-04-09 20:00:14 +0200

    🔨 [search] generate_audio script updated

--- voice/upload/generate_audio.sh
+++ voice/upload/generate_audio.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+echo generating EN audio...
 # english
 gtts-cli 'Welcome to Sera! For English audio press 1, for French audio press 2.' --output audio/en/welcome.wav
 gtts-cli "What type of Fonio do you want to sell? For Pon de Bore press 1, For Niatia press 2, For Banco Konkountre press 3, For any Other type press 4, To go back, press the asterisk." --output audio/en/subcategory_fonio.wav;
@@ -12,10 +13,14 @@ gtts-cli "You have selected the category:" --output audio/en/review_category.wav
 gtts-cli "You have selected the subcategory:" --output audio/en/review_subcategory.wav;
 gtts-cli "You have selected the quantity:" --output audio/en/review_quantity.wav;
 gtts-cli "You have selected the price:" --output audio/en/review_price.wav;
-gtts-cli "Something went wrong, try again later." --output audio/en/error.wav;
 gtts-cli "To confirm press 1, To listen again press the Hashtag, To go back press the Asterisk." --output audio/en/review_instructions.wav;
 gtts-cli "Your announcement has been uploaded! Thank your for using Sera!" --output audio/en/review_sent.wav;
+# - misc
+gtts-cli "kilos" --output audio/en/kilos.wav;
+gtts-cli "Something went wrong, try again later." --output audio/en/error.wav;
+echo OK
 
+echo generating FR audio...
 # french
 gtts-cli "Bienvenue à Sera! Pour l'audio anglais, appuyez sur 1, pour l'audio français appuyez sur 2." --lang fr --output audio/fr/welcome.wav
 gtts-cli "Quel type de Fonio voulez-vous vendre ? Pour Pon de Bore appuyez sur 1, Pour Niatia appuyez sur 2, Pour Banco Konkountre pressez 3, Pour tout autre type, appuyez sur 4, Pour revenir en arrière, appuyez sur l'astérisque." --lang fr --output audio/fr/subcategory_fonio.wav;
@@ -28,6 +33,9 @@ gtts-cli "Vous avez sélectionné la catégorie:" --lang fr --output audio/fr/re
 gtts-cli "Vous avez sélectionné type:" --lang fr --output audio/fr/review_subcategory.wav;
 gtts-cli "Vous avez sélectionné la quantité:" --lang fr --output audio/fr/review_quantity.wav;
 gtts-cli "Vous avez sélectionné le prix:" --lang fr --output audio/fr/review_price.wav;
-gtts-cli "Quelque chose s'est mal passé, réessayez plus tard." --lang fr --output audio/fr/error.wav;
 gtts-cli "Pour confirmer, appuyez sur 1, Pour réécouter, appuyez sur le Hashtag, Pour revenir en arrière, appuyez sur l'astérisque." --lang fr --output audio/fr/review_instructions.wav;
 gtts-cli "Votre annonce a été téléchargée! Merci d'avoir utilisé Sera!" --lang fr --output audio/fr/review_sent.wav;
+# - misc
+gtts-cli "kilos" --lang fr --output audio/fr/kilos.wav;
+gtts-cli "Quelque chose s'est mal passé, réessayez plus tard." --lang fr --output audio/fr/error.wav;
+echo OK
