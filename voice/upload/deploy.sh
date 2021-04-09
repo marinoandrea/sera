02c351ba64af44bd74d2fd55b0bdb6a4e870502c
Andrea Marino <am.marinoandrea@gmail.com>
2021-04-09 22:31:04 +0200

    🐛 folder scructure fix and scripts update

--- voice/upload/deploy.sh
+++ voice/upload/deploy.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 source .env
-lftp -u $VOXEO_USER,$VOXEO_PASSWORD -e "cd www/$VOXEO_APP; mput *.xml; cd en; mput en/*.xml; cd audio; mput en/audio/*.wav; cd ../../fr; mput fr/*.xml; cd audio; mput fr/audio/*.wav; quit" ftp.voxeo.net
+
+ftp_script="cd www/$VOXEO_APP; put index.xml; rm -rf en; mkdir en; cd en; mput en/*.xml; mkdir audio; cd audio; mput en/audio/*.wav; cd ../..; rm -rf fr; mkdir fr; cd fr; mput fr/*.xml; mkdir audio; cd audio; mput fr/audio/*.wav; quit;"
+lftp -u $VOXEO_USER,$VOXEO_PASSWORD -e "$ftp_script" ftp.voxeo.net
--- voice/upload/generate_audio.sh
+++ voice/upload/generate_audio.sh
@@ -3,6 +3,7 @@
 echo generating EN audio...
 # english
 gtts-cli 'Welcome to Sera! For English audio press 1, for French audio press 2.' --output en/audio/welcome.mp3
+gtts-cli 'What type of seed do you want to sell? For Fonio press 1, for Gombo press 2, for Mais press 3, for Sorgho press 4.' --output en/audio/category.mp3
 gtts-cli "What type of Fonio do you want to sell? For Pon de Bore press 1, For Niatia press 2, For Banco Konkountre press 3, For any Other type press 4, To go back, press the asterisk." --output en/audio/subcategory_fonio.mp3;
 gtts-cli "What type of Gombo do you want to sell? For Keleya press 1, For Sabalibougou press 2, For Gansourouni press 3, For any Other type press 4, To go back, press the asterisk." --output en/audio/subcategory_gombo.mp3;
 gtts-cli "What type of Mais do you want to sell? For Sotubaka press 1, For Nieleni press 2, For Dembaniouma press 3, For any Other type press 4, To go back, press the asterisk." --output en/audio/subcategory_mais.mp3;
@@ -23,6 +24,7 @@ echo OK
 echo generating FR audio...
 # french
 gtts-cli "Bienvenue à Sera! Pour l'audio anglais, appuyez sur 1, pour l'audio français appuyez sur 2." --lang fr --output fr/audio/welcome.mp3
+gtts-cli "Quel type de semences voulez-vous vendre ? Pour Fonio appuyez sur 1, pour Gombo appuyez sur 2, pour Mais appuyez sur 3, pour Sorgho appuyez sur 4." --lang fr --output fr/audio/category.mp3 
 gtts-cli "Quel type de Fonio voulez-vous vendre ? Pour Pon de Bore appuyez sur 1, Pour Niatia appuyez sur 2, Pour Banco Konkountre pressez 3, Pour tout autre type, appuyez sur 4, Pour revenir en arrière, appuyez sur l'astérisque." --lang fr --output fr/audio/subcategory_fonio.mp3;
 gtts-cli "Quel type de Gombo voulez-vous vendre ? Pour Keleya appuyez sur 1, Pour Sabalibougou appuyez sur 2, Pour Gansourouni pressez 3, Pour tout autre type, appuyez sur 4, Pour revenir en arrière, appuyez sur l'astérisque." --lang fr --output fr/audio/subcategory_gombo.mp3;
 gtts-cli "Quel type de Mais voulez-vous vendre ? Pour Sotubaka appuyez sur 1, Pour Nieleni appuyez sur 2, Pour Dembaniouma pressez 3, Pour tout autre type, appuyez sur 4, Pour revenir en arrière, appuyez sur l'astérisque." --lang fr --output fr/audio/subcategory_mais.mp3;
