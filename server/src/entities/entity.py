eb12a9172f23ebd1c202aa779323f0627e352b0d
Andrea Marino <am.marinoandrea@gmail.com>
2021-04-22 12:25:14 +0200

    🐛 [server] fixed entity default factory

--- server/src/entities/entity.py
+++ server/src/entities/entity.py
@@ -1,14 +1,13 @@
 from dataclasses import dataclass, field, fields
 from time import time
-
-from cuid import cuid
+from uuid import uuid4
 
 
 @dataclass(eq=True)
 class Entity:
-    id: str = field(default=cuid(), init=False)
-    created_at: int = field(default=int(time() * 1000), init=False)
-    updated_at: int = field(default=int(time() * 1000), init=False)
+    id: str = field(default_factory=lambda: str(uuid4()), init=False)
+    created_at: int = field(default_factory=lambda: int(time() * 1000), init=False)
+    updated_at: int = field(default_factory=lambda: int(time() * 1000), init=False)
 
     def to_json(self) -> dict:
         # type: ignore
